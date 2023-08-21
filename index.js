b7bdae21c602d80ce37584f5fc8a7c65f10efccf
subham gupta <0subhamgupta1@gmail.com>
2023-08-21 14:34:26 +0530

    aa

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 console.log(__dirname, "dirr");
+
+app.use(express.json());
+app.use(express.urlencoded());
 app.get("/privacy/social", (req, res) => {
-  res.sendFile(__dirname + "/html/social.html");
+    res.sendFile(__dirname + "/html/social.html");
+});
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-
-app.listen(8000)
+app.listen(8000);
