872a9811d99aa2308f2d2d4befc5ad46a91e9ab0
subham gupta <0subhamgupta1@gmail.com>
2023-08-21 15:27:06 +0530

    vercel file

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 // app.use(express.urlencoded());
-// app.get("/privacy/social", (req, res) => {
-//     res.sendFile(__dirname + "/html/social.html");
-// });
+app.get("/privacy/social", (req, res) => {
+    res.sendFile(__dirname + "/html/social.html");
+});
 app.get("/", (req, res) => {
   res.send("hello");
 });
